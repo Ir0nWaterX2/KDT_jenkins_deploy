275d3ce53f41e03ec6af673b5b12375eebcb0c6c
Ir0nWaterX2 <108861088+Ir0nWaterX2@users.noreply.github.com>
2024-07-11 14:31:31 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello...?";
+        return "hello Ir0n. ";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
